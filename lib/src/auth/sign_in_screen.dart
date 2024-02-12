7c5e580090fd70b1bcce7c943def6aeb9284b687
Rafael Alcides <rafaeldasilvaalcides@gmail.com>
2024-02-11 21:57:56 -0300

    create branch auth

---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(45),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.green,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
